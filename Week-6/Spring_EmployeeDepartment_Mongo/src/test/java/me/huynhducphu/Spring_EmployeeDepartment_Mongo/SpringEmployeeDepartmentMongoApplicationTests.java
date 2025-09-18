d738550432af33e86e3590c6e82de8cdc541073f
HuynhDucPhu2502 <huynhducphu2502@gmail.com>
2025-09-18 21:30:19 +0700

    updated

---
+++ Week-6/Spring_EmployeeDepartment_Mongo/src/test/java/me/huynhducphu/Spring_EmployeeDepartment_Mongo/SpringEmployeeDepartmentMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.huynhducphu.Spring_EmployeeDepartment_Mongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEmployeeDepartmentMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
